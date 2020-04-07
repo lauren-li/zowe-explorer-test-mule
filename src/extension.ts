77f04eb647cd975feba86be908671e8576ebdf08
Colin Stone <30794003+Colin-Stone@users.noreply.github.com>
2020-04-07 17:00:36 +0100

    Use a different profile
    Signed-off-by: Colin Stone <30794003+Colin-Stone@users.noreply.github.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,22 @@
 
 import * as vscode from 'vscode';
 
+export let EXTENDER_API;
 export function activate(context: vscode.ExtensionContext) {
-    vscode.commands.registerCommand("zowe.testmule.retcode", (node) => vscode.window.showInformationMessage("Return info: "+node.job.retcode));
+    vscode.commands.registerCommand("zowe.testmule.retcode", (node) => doSomeStuff(node));
+
+    const baseExt = vscode.extensions.getExtension('zowe.vscode-extension-for-zowe');
+    try {
+        if (baseExt && baseExt.exports) {
+            EXTENDER_API = baseExt.exports.getExplorerExtenderApi();
+            return;
+        }
+    } catch (error) {
+        vscode.window.showWarningMessage("Unable to access Zowe Explorer API");
+    }
+}
+
+async function doSomeStuff (node: vscode.TreeItem) {
+    const secondary = await EXTENDER_API.getRelatedProfile(node, "tso");
+    vscode.window.showInformationMessage("Accessed Profile "+ secondary.name);
 }
